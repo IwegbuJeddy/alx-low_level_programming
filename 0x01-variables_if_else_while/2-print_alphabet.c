1299f1fbb29059aeda6f1c50968a09a7202e0406
IwegbuJeddy <iJeddy5.0@gmail.com>
2022-07-01 09:41:40 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 0
+*/
+int main(void)
+{
+int n = 26;
+char letter = 'a';
+/* your code goes there */
+do {
+putchar(letter);
+letter++;
+n--;
+}
+while (n != 0);
+putchar('\n');
+return (0);
