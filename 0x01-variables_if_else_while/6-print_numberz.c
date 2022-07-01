2c35a572f2c82f389080940c43b7a9025f8c08a3
IwegbuJeddy <iJeddy5.0@gmail.com>
2022-07-01 11:42:12 -0700

    Prints single nums from 1 to 10 in new lines

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 0
+*/
+int main(void)
+{
+int i = 0;
+do {
+printf("%d\n", i);
+++i;
+} while (i != 10);
+return (0);
+}
