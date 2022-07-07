d4ed4060918c482d4965bb3c0f63f5cb4d7b02a0
IwegbuJeddy <iJeddy5.0@gmail.com>
2022-07-07 15:43:29 -0700

    print numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,12 +3,9 @@
 * mul - Multiplies two integers
 * @a : integer to be multiplied with b
 * @b : integer to be multiplied with a
-* @c : integer with result value
 * Return: returns the value of c, the product
 */
 int mul(int a, int b)
 {
-int c;
-c = a * b;
-return (c);
+return (a * b);
 }
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.c"
+
+void print_numbers(void)
+{
+int j = 0;
+for(; j < 10; j++)
+{
+_putchar(j + '0');
+_putchar('\n');
+}
+}
