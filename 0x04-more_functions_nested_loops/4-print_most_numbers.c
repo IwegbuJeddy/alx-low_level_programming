c01b2c7f54ef36bf3bdd28c7409a10cab8492ee8
IwegbuJeddy <iJeddy5.0@gmail.com>
2022-07-08 04:15:30 -0700

    prints nums from 0 to 14 10 times

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *print_most_numbers -> this prints most numbers
+ *Returns: Void.
 */
 void print_most_numbers(void)
 {
@@ -9,7 +11,7 @@ for (; ch < 58; ch++)
 {
 if (ch != 50 || ch != 52)
 {
-_putchar(ch);
+_putchar(ch + '0');
 }
 }
 _putchar ('\n');
