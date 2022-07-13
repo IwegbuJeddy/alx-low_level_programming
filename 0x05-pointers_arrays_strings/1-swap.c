e6f44b91e1d3671f655f4e29f13daf4374f5d7a5
IwegbuJeddy <iJeddy5.0@gmail.com>
2022-07-13 06:50:10 -0700

    Swap vars

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,5 @@
 #include "main.h"
-void swap_int(int *a, int *b)
-{
- /**
+/**
  * swap_int - swaps the values of two integers
  * @a: pointer 1
  * @b: pointer 2
@@ -12,5 +10,4 @@ void swap_int(int *a, int *b)
 int tmp = *a;
 *a = *b;
 *b = tmp;
-}
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #ifndef MAIN_H
 #define MAIN_H
 void reset_to_98(int *n);
